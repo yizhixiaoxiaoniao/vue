04bca1253de4fda9361ad497e9ccece2d1ff92c1
jinfeg <40690524+jinfeg@users.noreply.github.com>
2018-06-30 16:49:33 +0800

    Add files via upload

---
+++ src/components/container/Navigate.vue
@@ -0,0 +1,21 @@
+<template>
+  <Header style="height: 44px;">
+  </Header>
+</template>
+
+<script>
+export default {
+  name: 'Navigate',
+  data () {
+    return {
+    }
+  },
+  components: {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ src/components/pages/home/Demo1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="background:#eee;padding: 20px">
+        <Card :bordered="false">
+            <p slot="title">Hello Vue! Demo1</p>
+            <p>{{message}}</p>
+        </Card>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo1',
+  data () {
+    return {
+      message: 'This is home demo1!'
+    }
+  },
+  components: {},
+  methods: {},
+  created: function () {
+    this.$http.get('http://localhsot:9000', (response) => {
+      return response.json()
+    }).then((respData) => {
+      console.log(respData)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
