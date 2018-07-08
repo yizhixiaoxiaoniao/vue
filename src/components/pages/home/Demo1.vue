f2bfb35428cdcb8e17b40403244c6610fdce4e53
jinfeg <40690524+jinfeg@users.noreply.github.com>
2018-07-08 16:55:42 +0800

    Add files via upload

---
+++ src/components/pages/home/Demo1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div style="background:#eee;padding: 20px">
+        <Card :bordered="false">
+            <p slot="title">Hello Vue! Demo1</p>
+            <p>server response: {{message}}</p>
+        </Card>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo1',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  components: {},
+  methods: {},
+  created: function () {
+    this.$http.get('/api/test', (response) => {
+      return response.json()
+    }).then((respData) => {
+      this.message = respData.body.message
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
